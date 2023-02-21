8f4f68c9371722488840c57c23d1215bdc370daa
CFatwizC <124701543+CFatwizC@users.noreply.github.com>
2023-02-21 12:12:58 -0600

    Add files via upload

---
+++ EVA1_9_STACK_OVERFLOW/src/main/java/com/mycompany/eva1_9_stack_overflow/EVA1_9_STACK_OVERFLOW.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.eva1_9_stack_overflow;
+public class EVA1_9_STACK_OVERFLOW {
+    public static void main(String[] args) {
+        A();
+    }
+    
+    public static void A(){
+        A();
+    }
+}
