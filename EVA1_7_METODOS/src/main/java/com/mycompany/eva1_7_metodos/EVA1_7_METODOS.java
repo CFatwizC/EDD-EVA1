6e770458430f21d351f4e7e4ec945eeb67884df5
CFatwizC <124701543+CFatwizC@users.noreply.github.com>
2023-02-21 12:05:19 -0600

    Add files via upload

---
+++ EVA1_7_METODOS/src/main/java/com/mycompany/eva1_7_metodos/EVA1_7_METODOS.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.eva1_7_metodos;
+
+/**
+ *
+ * @author Derek Franco
+ */
+public class EVA1_7_METODOS {
+
+    public static void main(String[] args) {
+        myMethod();
+    }
+    
+    public static void myMethod(){
+        System.out.println("Hola soy un metodo, la neta doy demasiada lata, pero, pues toca verme no jaja salu2");
+    }
+}
