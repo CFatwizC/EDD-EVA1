97550f2a9293088f2649cbf71f6bd8cb0aa64cd4
CFatwizC <124701543+CFatwizC@users.noreply.github.com>
2023-02-23 21:51:26 -0600

    Add files via upload

---
+++ EVA1_13_ACLANCE/src/main/java/com/mycompany/eva1_13_aclance/EVA1_13_ACLANCE.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.eva1_13_aclance;
+public class EVA1_13_ACLANCE {
+
+    public static void main(String[] args) {
+        int x =100;
+        for(int i=0; i<10; i++ ){
+            System.out.println("i= " + i);
+            int j=100;
+            System.out.println("j = " + j);
+            System.out.println("x = " + x);
+            {
+                int z=1000;
+                System.out.println("z= " + z);
+            }
+        }
+    }
+    
+    public static void xd(){
+        System.out.println("x= ");
+    }
+}
