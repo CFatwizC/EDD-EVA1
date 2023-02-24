eeb006a367c6159f601ce56553bd0a52273bee9a
CFatwizC <124701543+CFatwizC@users.noreply.github.com>
2023-02-24 13:31:50 -0600

    Add files via upload

---
+++ EVA1_15_SOBRECARGA/src/eva1_15_sobrecarga/EVA1_15_SOBRECARGA.java
@@ -0,0 +1,15 @@
+package eva1_15_sobrecarga;
+public class EVA1_15_SOBRECARGA {
+    public static void main(String[] args) {
+        /*Diferentes tipos de println: 10 types
+        metodos para imprimir diferentes datos
+        para crear la sobrecarga de methods, la firma debe ser unica
+        */
+        
+        System.out.println("Holi");
+        System.out.println(100);
+        System.out.println(true);
+        System.out.println(args);
+    }
+    
+}
