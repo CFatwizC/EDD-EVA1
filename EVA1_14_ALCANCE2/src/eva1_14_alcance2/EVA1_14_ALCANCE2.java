20d1a4b5dc4e662560c9b936a0bcdc8e4afe6d16
CFatwizC <124701543+CFatwizC@users.noreply.github.com>
2023-02-24 12:20:58 -0600

    Add files via upload

---
+++ EVA1_14_ALCANCE2/src/eva1_14_alcance2/EVA1_14_ALCANCE2.java
@@ -0,0 +1,14 @@
+package eva1_14_alcance2;
+public class EVA1_14_ALCANCE2 {
+    public static void main(String[] args) {
+        int x=100;
+        for(int i=0; i<10; i++){
+            int y=100;
+            if(i==5){
+                
+            }
+        }
+        int y=100;
+    }
+    
+}
